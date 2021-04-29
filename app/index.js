412965a69121e9212fcb44cdab95f5e7a2c44e7b
eaccmk <eaccmk@users.noreply.github.com>
2021-04-29 22:13:25 +1000

    Update index.js - launch screen
    The first screen will have a default message with some instructions for user. (can be internationalized/localized if you like)

--- app/index.js
+++ app/index.js
@@ -4,6 +4,17 @@
 import document from "document";
 import * as messaging from "messaging";
 
+
+function initialize() {
+    //Set default screen with some meesage and instruction ( something better than nothing)
+  var index = 1;
+  var article = document.getElementById("article_" + index);
+  var extract = article.getElementsByClassName("extract")[0];
+  var title = article.getElementsByClassName("title")[0];
+  
+  title.text = "Loading now .... ";
+  extract.text = "Getting latest data for you ⌛ \n\nMake sure your Internet is On ✔ \nand \nLocation access is granted to fitbit app on phone ⚙.";
+}
 // Listen for the onmessage event
 messaging.peerSocket.onmessage = function(evt) {
 
@@ -29,3 +40,4 @@ messaging.peerSocket.onopen = function() {
   messaging.peerSocket.send("Hi!");
 }
 
+initialize();
